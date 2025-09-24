88a661e16e79472d6e1ea2eadf42ad9aef75b0bc
Ateeb Khan <ateebkhan997@gmail.com>
2025-09-24 15:49:43 +0500

    feat(shared): Add HTTP service with Axios support
    - Introduced  in the shared module for handling HTTP requests using .
    - Added methods to  for GET, POST, PUT, PATCH, and DELETE operations, with error handling and logging.
    - Updated  to import  and provide .
    - Installed  and  packages in the project.

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,9 +1,11 @@
 import { Global, Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 import { HttpService } from './services/http.service';
 import { CommonService } from './services/common.service';
 
 @Global()
 @Module({
+	imports: [HttpModule],
 	providers: [CommonService, HttpService],
 	exports: [CommonService, HttpService],
 })
